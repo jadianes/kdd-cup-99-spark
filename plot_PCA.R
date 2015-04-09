5a32b26f279dbee71ee6e61399a50d5cc9e852bd
Jose A Dianes <jadianes@gmail.com>
2015-04-09 19:40:54 +0100

    now using ggplot2

--- plot_PCA.R
+++ plot_PCA.R
@@ -2,10 +2,10 @@
 clustering <- read.csv("clustering_results.csv", header=FALSE)
 
 # perform PCA
-clustering_pca <- prcomp(clustering, scale = TRUE)
+clustering_pca <- prcomp(clustering)
 
 # Extract components 1 and 2
 clustering_pca_1_2 <- clustering_pca$x[, 1:2]
 
 # plot - asp = 1 gives equal scaling to x and y axes
-plot(clustering_pca_1_2, asp = 1) 
+plot(clustering_pca_1_2, col=clustering$V1, type = 'p',pch = 16, asp = 1) 

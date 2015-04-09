7fd0a34e3fcdc61323099868dfbbe4a4579297d9
Jose A Dianes <jadianes@gmail.com>
2015-04-09 19:41:45 +0100

    now using ggplot2

--- plot_PCA.R
+++ plot_PCA.R
@@ -8,4 +8,8 @@ clustering_pca <- prcomp(clustering)
 clustering_pca_1_2 <- clustering_pca$x[, 1:2]
 
 # plot - asp = 1 gives equal scaling to x and y axes
-plot(clustering_pca_1_2, col=clustering$V1, type = 'p',pch = 16, asp = 1) 
+library(ggplot2)
+pca_df = data.frame(clustering_pca_1_2)
+pca_df$cluster <- as.factor(clustering$V1)
+ggplot(data=pca_df, aes(x=PC1, y=PC2, color=cluster)) +
+    geom_point()

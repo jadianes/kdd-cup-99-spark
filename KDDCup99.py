1c89e58ae2fa0f0b7a20da1aaece894515827524
cluster <jadianes@gmail.com>
2015-04-09 18:54:40 +0100

    new commands + ignore

--- KDDCup99.py
+++ KDDCup99.py
@@ -41,8 +41,8 @@ def dist_to_centroid(datum, clusters):
 
 
 def clustering_score(data, k):
-    clusters = KMeans.train(data, k, maxIterations=10, runs=10, initializationMode="random")
-    return data.map(lambda datum: dist_to_centroid(datum, clusters)).mean()
+    clusters = KMeans.train(data, k, maxIterations=10, runs=5, initializationMode="random")
+    return (k, clusters, data.map(lambda datum: dist_to_centroid(datum, clusters)).mean())
 
 
 if __name__ == "__main__":
@@ -65,24 +65,30 @@ if __name__ == "__main__":
     sorted_labels = OrderedDict(sorted(label_counts.items(), key=lambda t: t[1], reverse=True))
     print "Different labels and their interaction counts: "
     for label, count in sorted_labels.items():
-    	print label, count
+        print label, count
 
     # Prepare data for clustering input
     # the data contains non-numeric features, we want to exclude them since
     # k-means works with numeric features. These are the first three and the last
     # column in each data row
     parsed_data = raw_data.map(parse_interaction)
-
     parsed_data_values = parsed_data.values().cache()
 
     # Evaluate values of k from 5 to 40
-    print "Calculate scores for different k values (5 to 40)"
-    scores = map(lambda k: (k, clustering_score(parsed_data_values, k)), range(5,41,5))
+    print "Calculate scores for different k values (10 to 50)"
+    scores = map(lambda k: clustering_score(parsed_data_values, k), range(10,101,10))
 
     # print scores
     for score in scores:
-        print score
+        print score[0], score[2]
 
-    min_k = min(scores, key=lambda x: x[1])[0]
+    min_k = min(scores, key=lambda x: x[2])[0]
     print "Best k value is ", min_k
+
+    # Use the best model to assign a cluster to each datum
+    best_model = min(scores, key=lambda x: x[2])[1]
+    cluster_assignments_sample = parsed_data_values.map(lambda datum: str(best_model.predict(datum))+","+",".join(map(str,datum))).sample(False,0.05)
+
+    # Save assignment sample to file
+    cluster_assignments_sample.saveAsTextFile("sample")
 
---
+++ run_kdd_10.sh
@@ -0,0 +1,1 @@
+~/spark-1.2.1-bin-hadoop2.4/bin/spark-submit --master spark://169.254.206.2:7077 --total-executor-cores 10 --executor-memory 4g /nfs/programs/KDDCup99/kdd-cup-99-spark/KDDCup99.py /nfs/data/KDD99/kddcup.data_10_percent

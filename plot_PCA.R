c8860a57771e410a3970cd0a196fec455b75cabb
Jose A Dianes <jadianes@gmail.com>
2015-04-10 10:44:30 +0100

    playing with jitter

--- plot_PCA.R
+++ plot_PCA.R
@@ -7,9 +7,9 @@ clustering_pca <- prcomp(clustering)
 # Extract components 1 and 2
 clustering_pca_1_2 <- clustering_pca$x[, 1:2]
 
-# plot - asp = 1 gives equal scaling to x and y axes
+# if using basic plot - asp = 1 gives equal scaling to x and y axes
 library(ggplot2)
 pca_df = data.frame(clustering_pca_1_2)
 pca_df$cluster <- as.factor(clustering$V1)
 ggplot(data=pca_df, aes(x=PC1, y=PC2, color=cluster)) +
-    geom_point()
+    geom_point(alpha=.5, size = 4, position = position_jitter(w = 0.8, h = 0.8))

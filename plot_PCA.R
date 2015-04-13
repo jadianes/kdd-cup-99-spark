6027f389971c67cb256c029efce988b15f579049
Jose A Dianes <jadianes@gmail.com>
2015-04-13 22:18:03 +0100

    generated some plot

--- plot_PCA.R
+++ plot_PCA.R
@@ -12,4 +12,5 @@ library(ggplot2)
 pca_df = data.frame(clustering_pca_1_2)
 pca_df$cluster <- as.factor(clustering$V1)
 ggplot(data=pca_df, aes(x=PC1, y=PC2, color=cluster)) +
-    geom_point(alpha=.5, size = 4, position = position_jitter(w = 0.8, h = 0.8))
+    geom_point(alpha=.5, size = 3, position = position_jitter(w = 0.1, h = 0.5)) +
+    coord_cartesian(ylim = c(-10,1))
